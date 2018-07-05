b01b1a218bdc8a0aa1bb2ad121b5d4d070dd24c9
David Montoya <david.montoya@alejandria-consulting.com>
2018-07-05 16:28:18 -0500

    avances estructura

---
+++ electron/ts_app/dt/App.d.ts
@@ -0,0 +1,8 @@
+declare var proccessChildList: any;
+declare var globalRoutes: any;
+declare var globalOptions: any;
+declare var globalForms: any;
+declare var globalMenus: any;
+declare var Global: any;
+declare var Handsontable:any;
+declare var HotRegsTp2:any;
